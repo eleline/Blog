76af8722456c49a64e62f1f5f7f6ea423509fc66
eleline <ggggepicstar@gmail.com>
2019-11-11 15:27:39 +0900

    card__bodyの文字色変更

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -60,13 +60,13 @@ export default {
   }
 
   &__body {
-    color: rgba(#292929, 0.64);
+    color: rgba(#292929, 0.78);
     display: -webkit-box;
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 3;
     overflow: hidden;
     @media (prefers-color-scheme: dark) {
-      color: rgba(#fcfcfc, 0.64);
+      color: rgba(#fcfcfc, 0.78);
     }
   }
 }
